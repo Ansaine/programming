68c664f5748d019ff4d8a11d96c9a6f3f8fe5515
Angshuman <dsangshuman@gmail.com>
2024-07-17 13:03:56 +0530

    redis commands added

---
+++ redis/cli_commands.sh
@@ -0,0 +1,10 @@
+// after connecting with cli in terminal
+
+SET key value 
+GET key         // value will output
+GET *           // all keys
+SETNX key value // if key exists, it cant update
+TYPE key        // give datatype
+EXISTS key
+TTL key         // returns -1 for no ttl, -2 for key doesnt exist
+EXPIRE key <time im secs>
