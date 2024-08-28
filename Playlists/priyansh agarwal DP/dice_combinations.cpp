5b994c10881c68900d49a6e2672b5d1aed6fa39b
Angshuman <dsangshuman@gmail.com>
2024-08-28 13:49:24 +0530

    cleaned repo

---
+++ Playlists/priyansh agarwal DP/dice_combinations.cpp
@@ -0,0 +1,23 @@
+
+#define mod 1e9+7
+#define fo(i,n) 	for(int i = 0; i<n; i++)
+#define fok(i,k,n) 	for(int i = k; i<n; i++)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+
+int t;
+cin>>t;
+
+while(t--)
+{   
+
+    
+}
+
+return 0;
+}
